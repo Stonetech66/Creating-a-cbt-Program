8603cccd3daa0cbb90230d117e0f1c6172a4172f
Stonetech66 <106387735+Stonetech66@users.noreply.github.com>
2022-07-07 14:53:59 +0100

    Add files via upload

---
+++ creating cbt Program/app.py
@@ -0,0 +1,37 @@
+#
+
+class cbt:
+   def __init__(self, question , answer):
+     self.question= question
+     self.answer= answer
+     
+<<<<<<< HEAD
+"""
+Edit the question_part and input your own questions with the answers
+"""     
+=======
+     
+     
+>>>>>>> 4b54640fb05da66dcba84d94b368e75f637d1c82
+     
+question_part=[
+"who is the  premier league winner of 2021/2022\n(a) chelsea\n(b) mancity\n(c) liverpool\n\n",
+"who is the  champions league winner of 2021/2022\n(a) liverpool\n(b) mancity\n(c) realmadrid\n\n",
+"who is the president of America\n(a) joe biden\n(b) donald trump\n(c)hillary clinton\n\n",
+]
+
+Answers=[
+cbt(question_part[0],"b"),
+cbt(question_part[1],"c"),
+cbt(question_part[2],"a"),
+]
+
+
+def run_cbt(Answers):
+   score=0
+   for cbt in Answers:
+     answer=input (cbt.question)
+     if answer== cbt.answer:
+       score += 1
+   print("your score is ",score," / ",len(Answers))
+run_cbt(Answers)
